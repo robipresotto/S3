d5a6ef4eff5097776a4e94f041aaf95f433f057a
Zsolt Varadi <vzsg@users.noreply.github.com>
2020-05-22 14:08:36 +0200

    Subvert test expectations now that URLComponents is not lowercasing query parameters

--- Tests/S3Tests/S3Tests.swift
+++ Tests/S3Tests/S3Tests.swift
@@ -175,7 +175,7 @@ class S3Tests: BaseTestCase {
 		let expectedCanonRequest = [
 			"GET",
 			"/test.txt",
-			"x-amz-algorithm=AWS4-HMAC-SHA256&x-amz-credential=AKIAIOSFODNN7EXAMPLE%2F20130524%2Fus-east-1%2Fs3%2Faws4_request&x-amz-date=20130524T000000Z&x-amz-expires=86400&x-amz-signedheaders=host",
+			"X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIOSFODNN7EXAMPLE%2F20130524%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20130524T000000Z&X-Amz-Expires=86400&X-Amz-SignedHeaders=host",
 			"host:examplebucket.s3.amazonaws.com",
 			"",
 			"host",
@@ -183,14 +183,14 @@ class S3Tests: BaseTestCase {
 			].joined(separator: "\n")
 
         let (canonRequest, _) = try! signer.presignedURLCanonRequest(.GET, dates: overridenDate, expiration: Expiration.custom(86400), url: requestURL, region: region, headers: ["Host": requestURL.host ?? Region.usEast1.host])
-		
+
 		XCTAssertEqual(expectedCanonRequest, canonRequest)
-		
+
 		let expectedStringToSign = [
 			"AWS4-HMAC-SHA256",
 			"20130524T000000Z",
 			"20130524/us-east-1/s3/aws4_request",
-			"414cf5ed57126f2233fac1f7b5e5e4b8dcf58040010cb69d21f126d353d13df7"
+			"3bfa292879f6447bbcda7001decf97f4a54dc650c8942174ae0a9121cf58ad04"
 		].joined(separator: "\n")
 		
 		let stringToSign = try! signer.createStringToSign(canonRequest, dates: overridenDate, region: region)
@@ -199,14 +199,14 @@ class S3Tests: BaseTestCase {
         NSLog("stringToSign: \(stringToSign)")
 		XCTAssertEqual(expectedStringToSign, stringToSign)
 		
-		let expectedSignature = "17594f59285415a5be4debfcf5227a2d78b7c2634442b7ab816cace9333ec989"
+		let expectedSignature = "aeeed9bbccd4d02ee5c0109b86d86835f995330da4c265957d157751f604d404"
 		
 		let signature = try! signer.createSignature(stringToSign, timeStampShort: overridenDate.short, region: region)
 		
 		XCTAssertEqual(expectedSignature, signature)
 
 		
-		let expectedURLString = "https://examplebucket.s3.amazonaws.com/test.txt?x-amz-algorithm=AWS4-HMAC-SHA256&x-amz-credential=AKIAIOSFODNN7EXAMPLE%2F20130524%2Fus-east-1%2Fs3%2Faws4_request&x-amz-date=20130524T000000Z&x-amz-expires=86400&x-amz-signedheaders=host&x-amz-signature=17594f59285415a5be4debfcf5227a2d78b7c2634442b7ab816cace9333ec989"
+		let expectedURLString = "https://examplebucket.s3.amazonaws.com/test.txt?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIOSFODNN7EXAMPLE/20130524/us-east-1/s3/aws4_request&X-Amz-Date=20130524T000000Z&X-Amz-Expires=86400&X-Amz-SignedHeaders=host&X-Amz-Signature=aeeed9bbccd4d02ee5c0109b86d86835f995330da4c265957d157751f604d404"
 
         let presignedURL = try! signer.presignedURL(for: .GET, url: requestURL, expiration: Expiration.custom(86400), region: region, headers: [:], dates: overridenDate)
 
